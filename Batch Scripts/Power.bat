d3720916cfe747898d504d59606bdb380783302f
Shafkat Hasan <shafkathasan2@gmail.com>
2023-10-29 21:35:57 +0600

    Update Power.bat

--- Batch Scripts/Power.bat
+++ Batch Scripts/Power.bat
@@ -10,7 +10,7 @@ ECHO "4 = Shutdown"
 SET /p option=Choose one option - 
 
 IF %option%==1 SHUTDOWN /l
-IF %option%==2 SHUTDOWN -r -t 10
+IF %option%==2 SHUTDOWN -r -t 5
 IF %option%==3 SHUTDOWN /h
 IF %option%==4 SHUTDOWN /s /f /t 0
 
